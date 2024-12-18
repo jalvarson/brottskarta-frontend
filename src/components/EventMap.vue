057e63149c78a926ca6b24193a353c8d52b25048
Jonas Alvarson <jonas.alvarsson@gmail.com>
2024-12-18 21:30:04 +0100

    Add vue3-google-map dependency and integrate GoogleMap component in EventMap

--- src/components/EventMap.vue
+++ src/components/EventMap.vue
@@ -1,11 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { GoogleMap } from 'vue3-google-map'
+
+const defaultCenter = { lat: 40.689247, lng: -74.044502 }
+</script>
 
 <template>
   <div class="event-map-container">
     <div class="event-card-holder">
       <EventCard />
     </div>
-    <div class="event-map"></div>
+    <GoogleMap
+      api-key="AIzaSyDeWFYlg796lkBGlnPku6Waa2g8dKnYPoA"
+      class="event-map"
+      :center="defaultCenter"
+      :zoom="8"
+    />
   </div>
 </template>
 
@@ -45,8 +54,10 @@
   min-height: 100%;
   background-color: $surface-container-high;
   border-radius: 12px;
-  background-image: url('@/assets/map-placeholder.png');
-  background-size: cover;
-  background-position: center;
+  overflow: hidden !important;
+
+  .mapdiv {
+    flex: 1 !important;
+  }
 }
 </style>
