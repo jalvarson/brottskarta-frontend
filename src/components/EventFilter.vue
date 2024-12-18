49b933037318c17f391c67c59e26e1455e847acd
Jonas Alvarson <jonas.alvarsson@gmail.com>
2024-12-18 20:55:58 +0100

    Add EventFilter component with styling and layout

---
+++ src/components/EventFilter.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="event-filter-container"></div>
+</template>
+
+<style lang="scss" scoped>
+.event-filter-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 64px;
+  background-color: $surface-container;
+  border-radius: 12px;
+  margin-bottom: 16px;
+}
+</style>
